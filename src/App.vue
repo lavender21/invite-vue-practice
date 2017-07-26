6d0317e4123cd234d44f301ef98308fdb421561e
rtning <1573872488@qq.com>
2017-07-26 16:20:10 +0800

    add titleView component

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,56 @@
 <template>
-    <div id="app">
-        hello
+    <div id="app" class="box">
+        <titleView></titleView>
     </div>
 </template>
 
 <script>
 
+import titleView from './titleView.vue'
+
 export default {
     name: 'app',
+    components: { titleView },
     data () {
         return {}
     }
 }
 </script>
 
-<style>
+<style lang="scss">
+    $bg-color:rgba(0,0,0,0.75);
+    $box-bg-color:#fff;
+    $main-font-family: 'Arial';
 
+    @mixin border-radius($radius){
+        -webkit-border-radius:$radius;
+        -moz-border-radius:$radius;
+        border-radius:$radius;
+    }
+
+    html, body, div,h1,h2 {
+        margin: 0;
+        padding: 0;
+    }
+
+    html {
+        height: 100%;
+    }
+
+    body {
+        height: 100%;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        background: $bg-color;
+        font-family: $main-font-family;
+    }
+
+    .box {
+        width: 28em;
+        height: 36.8em;
+        padding: 1.9em 2.9em;
+        background: $box-bg-color;
+        @include border-radius(4px);
+    }
 </style>
---
+++ src/titleView.vue
@@ -0,0 +1,41 @@
+<template>
+    <header>
+        <h1 class="title">Invite people to Dribbble</h1>
+        <h2 class="subtitle"><span>{{count}}</span>invites available</h2>
+    </header>
+</template>
+
+<script>
+    export default {
+        name: 'titleView',
+        data () {
+            return {
+                count: 0
+            }
+        }
+    }
+</script>
+
+<style lang="scss">
+    $subtitle-color:#999999;
+    .center{
+        text-align: center;
+        vertical-align: center;
+    }
+
+    h1.title {
+        @extend .center;
+        font-size: 1.5em;
+        height: 2.6em;
+        line-height: 2.9em;
+        letter-spacing: 2px;
+    }
+
+    h2.subtitle {
+        @extend .center;
+        font-size: 0.8em;
+        color: $subtitle-color;
+        line-height: 1em;
+        letter-spacing: 1px;
+    }
+</style>
