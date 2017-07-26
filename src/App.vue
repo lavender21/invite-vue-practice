5b06f1eacb80e6f82ff6796ab04e6fbffce012ae
rtning <1573872488@qq.com>
2017-07-26 16:03:06 +0800

    init project

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+    <div id="app">
+        hello
+    </div>
+</template>
+
+<script>
+
+export default {
+    name: 'app',
+    data () {
+        return {}
+    }
+}
+</script>
+
+<style>
+
+</style>
